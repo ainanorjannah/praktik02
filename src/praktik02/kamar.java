dac41bba2e663e2c7871b0ccc42e90a891508725
ainanorjannah <ananorjannah0852@gmail.com>
2018-03-28 22:34:13 +0800

    membuat attribut dan behavior

--- src/praktik02/kamar.java
+++ src/praktik02/kamar.java
@@ -6,5 +6,12 @@
  * @author PC
  */
 public class kamar {
+    double luas;
+    double lebar;
+    
+    void cetakInfo(){
+        System.out.println("Luas             :"+luas);
+        System.out.println("Lebar            :"+lebar);
+    }
     
 }
